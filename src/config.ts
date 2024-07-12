b8eb5d5c7d53a7b5924b26445a2c7943c0af22f1
shiyi <529977910@qq.com>
2024-07-12 16:11:32 +0800

    :tada: Initial commit.

--- src/config.ts
+++ src/config.ts
@@ -9,13 +9,13 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'Fuwari',
   subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
+    enable: true,
     src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
   },
---
+++ src/content/posts/test.md
@@ -0,0 +1,9 @@
+---
+title: test
+published: 2024-07-12
+description: ''
+image: ''
+tags: []
+category: ''
+draft: false 
+---
